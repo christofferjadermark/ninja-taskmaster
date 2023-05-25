f55e6a62d74233809d3d06b70dfb46e6d5066124
Joel Bränninger <joel.branninger@live.se>
2023-05-25 08:59:53 +0200

    merge

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,47 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict';
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-const pg_1 = require("pg");
-const cors_1 = __importDefault(require("cors"));
-const express_1 = __importDefault(require("express"));
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const dotenv_1 = __importDefault(require('dotenv'));
+const pg_1 = require('pg');
+const cors_1 = __importDefault(require('cors'));
+const express_1 = __importDefault(require('express'));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
@@ -170,8 +196,9 @@ app.post('/login', (request, response) => __awaiter(void 0, void 0, void 0, func
     console.log(request.body + 'body');
     console.log(email, password + 'jjj');
     try {
+        yield pool.connect();
         const query = 'SELECT * FROM users WHERE username = $1 AND password = $2';
-        const values = [email, password];
+        const values = [username, password];
         const result = yield pool.query(query, values);
         // const test = await pool.query('SELECT * FROM activities');
         // console.log(test.rows);
@@ -184,9 +211,72 @@ app.post('/login', (request, response) => __awaiter(void 0, void 0, void 0, func
             response.status(401).json({ message: 'Ogiltiga inloggningsuppgifter.' });
         }
     }
-    catch (error) {
-        console.error('Fel vid anslutning:', error);
-        response.status(500).send('Ett fel uppstod vid anslutning till databasen.');
+  })
+);
+// Delete a task by ID
+app.delete('/api/tasks/:id', (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    try {
+      const deleteTaskQuery = 'DELETE FROM tasks WHERE id = $1';
+      yield pool.query(deleteTaskQuery, [id]);
+      res.sendStatus(204);
+    } catch (error) {
+      console.error('Failed to delete task:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  })
+);
+// Christoffers kod för att uppdatera aktivitet, fungerar inte än
+// app.patch('/update', async (request, response) => {
+//   const { activity_id, title, description, date } = request.body;
+//   console.log(activity_id, title, description, date);
+//   const user = 'SELECT * FROM users WHERE user_id = $1'
+//   try {
+//     const query =
+//       'UPDATE activities SET title = $1, description = $2, due_date = $3 WHERE activity_id = $4';
+//     const values = [title, description, date, activity_id];
+//     await pool
+//       .query(query, values)
+//       .then(() => {
+//         response.status(201).send('Aktivitet Uppdaterad!');
+//       })
+//       .catch((error: Error) => {
+//         console.error('Fel vid skapande av konto:', error);
+//         response
+//           .status(500)
+//           .send('Ett fel uppstod vid uppdatering av aktiviteten.');
+//       });
+//   } catch (error) {
+//     console.error('Fel vid anslutning:', error);
+//     response.status(500).send('Ett fel uppstod vid anslutning till databasen.');
+//   }
+// });
+app.post('/login', (request, response) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = request.body;
+    console.log(request.body + 'body');
+    console.log(email, password + 'jjj');
+    try {
+      const query = 'SELECT * FROM users WHERE username = $1 AND password = $2';
+      const values = [email, password];
+      const result = yield pool.query(query, values);
+      // const test = await pool.query('SELECT * FROM activities');
+      // console.log(test.rows);
+      console.log(JSON.stringify(result.rows) + 'Rows');
+      if (result.rows.length > 0) {
+        console.log(result.rows.length);
+        response.json(result.rows);
+      } else {
+        response
+          .status(401)
+          .json({ message: 'Ogiltiga inloggningsuppgifter.' });
+      }
+    } catch (error) {
+      console.error('Fel vid anslutning:', error);
+      response
+        .status(500)
+        .send('Ett fel uppstod vid anslutning till databasen.');
     }
 }));
 const parseUrlEncodedMiddleware = express_1.default.urlencoded({ extended: false });
@@ -194,6 +284,7 @@ app.post('/create', parseUrlEncodedMiddleware, (request, response) => __awaiter(
     const { userName, email, password } = request.body;
     console.log(userName, email, password);
     try {
+        yield pool.connect();
         const query = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)';
         const values = [userName, email, password];
         console.log(userName + '53 ');
@@ -202,19 +293,21 @@ app.post('/create', parseUrlEncodedMiddleware, (request, response) => __awaiter(
             .then(() => {
             response.status(201).send('Konto skapat!');
         })
-            .catch((error) => {
-            console.error('Fel vid skapande av konto:', error);
-            response.status(500).send('Ett fel uppstod vid skapandet av kontot.');
+        .catch((error) => {
+          console.error('Fel vid skapande av konto:', error);
+          response.status(500).send('Ett fel uppstod vid skapandet av kontot.');
         });
+    } catch (error) {
+      console.error('Fel vid anslutning:', error);
+      response
+        .status(500)
+        .send('Ett fel uppstod vid anslutning till databasen.');
     }
-    catch (error) {
-        console.error('Fel vid anslutning:', error);
-        response.status(500).send('Ett fel uppstod vid anslutning till databasen.');
-    }
-}));
+  })
+);
 // });
 app.listen(8080, () => {
-    console.log('Webbtjänsten kan nu ta emot anrop.');
+  console.log('Webbtjänsten kan nu ta emot anrop.');
 });
 // CREATE TABLE users (
 //   user_id SERIAL PRIMARY KEY,
